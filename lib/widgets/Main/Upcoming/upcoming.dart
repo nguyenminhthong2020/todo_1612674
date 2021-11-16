8d0fa42c2a98bb3920bacdee33b83ff1363d541a
nguyenminhthong2020 <1612674@student.hcmus.edu.vn>
2021-11-16 22:56:47 +0700

    upcoming

--- lib/widgets/Main/Upcoming/upcoming.dart
+++ lib/widgets/Main/Upcoming/upcoming.dart
@@ -17,7 +17,7 @@ class _UpcomingState extends State<Upcoming> {
   final _todos = <TodoItemModel>[
     TodoItemModel(0, "title0", "descrip0", "2021-11-16", "21:50", true),
     TodoItemModel(1, "title1", "descrip1", "2021-11-16", "21:50", false),
-    TodoItemModel(2, "title2", "descrip2", "2021-11-16", "22:10", false),
+    TodoItemModel(2, "title2", "descrip2", "2021-11-17", "22:10", false),
     TodoItemModel(3, "title3", "descrip3", "2021-11-17", "21:50", true),
     TodoItemModel(4, "title4", "descrip4", "2021-11-17", "21:50", false),
     TodoItemModel(5, "title5", "descrip5", "2021-11-18", "22:10", false),
@@ -29,18 +29,19 @@ class _UpcomingState extends State<Upcoming> {
   Widget build(BuildContext context) {
     Widget _buildTodoAllList() {
       String _searchQueryLower = _searchQuery.toLowerCase();
-      DateTime _date = (new DateTime.now());
+      //DateTime _date = (new DateTime.now());
+      String _date = (new DateTime.now()).toString().substring(0, 10);
       List<Widget> listTodo = [];
       for (var todo in _todos) {
         if (todo.title.contains(_searchQueryLower) ||
             todo.description.contains(_searchQueryLower)) {
           //1998-10-27
-          int y = int.parse(todo.date.substring(0, 4));
-          int m = int.parse(todo.date.substring(5, 7));
-          int d = int.parse(todo.date.substring(8, 10));
-
-          if (_date.isBefore(DateTime(y, m, d))) {
-            listTodo.add(TodoItem(
+          // int y = int.parse(todo.date.substring(0, 4));
+          // int m = int.parse(todo.date.substring(5, 7));
+          // int d = int.parse(todo.date.substring(8, 10));
+          
+          if(todo.date.compareTo(_date) == 1){
+              listTodo.add(TodoItem(
               id: todo.id,
               title: todo.title,
               description: todo.description,
@@ -49,6 +50,16 @@ class _UpcomingState extends State<Upcoming> {
               isDone: todo.isDone,
             ));
           }
+          // if (_date.isBefore(DateTime(y, m, d))) {
+          //   listTodo.add(TodoItem(
+          //     id: todo.id,
+          //     title: todo.title,
+          //     description: todo.description,
+          //     date: todo.date,
+          //     time: todo.time,
+          //     isDone: todo.isDone,
+          //   ));
+          // }
         }
       }
       return Column(
